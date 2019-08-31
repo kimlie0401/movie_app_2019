1f3db1fbb4180e4c3e65e3c076dc05e319bc0ece
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-08-31 15:46:46 -0700

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
+    rating: 4.8,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
     id: 2,
+    rating: 4.5,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
   },
   {
     id: 3,
+    rating: 4.2,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
   },
   {
     id: 4,
+    rating: 4.1,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
   },
   {
     id: 5,
+    rating: 5.0,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
-function Food({ name, pic }) {
+function Food({ name, rate, pic }) {
   return (
     <div>
       <h2>I love {name}</h2>
+      <h2>{rate} / 5.0</h2>
       <img src={pic} alt={name} />
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rate: PropTypes.number.isRequired
+};
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} pic={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          pic={dish.image}
+          rate={dish.rating}
+        />
       ))}
     </div>
   );
